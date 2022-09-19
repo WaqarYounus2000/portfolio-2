c6933f2303deea51d6bac59783a7860090a4a878
WaqarYounus2000 <83652319+WaqarYounus2000@users.noreply.github.com>
2022-09-19 23:21:50 +0500

    just initiated

---
+++ portfolio 2/app.js
@@ -0,0 +1,99 @@
+// document.getElementById('button1').onclick = ()=>{
+//     let page1 = document.getElementById('page1ID');
+//     page1.classList.add("page1style");
+//     console.log('krdy hai')
+
+//     let page2 = document.getElementById('page2ID');
+//     page2.classList.add('page2style');
+//     console.log('ye b hogya')
+
+// }
+var page1 = document.getElementById('page1ID');
+var page2 = document.getElementById('page2ID');
+
+document.getElementById('button1').onclick = ()=>{
+    page1.style.height = '0vh';
+    page2.style.height = '100vh';
+    
+    
+}
+
+document.getElementById('button2').onclick = ()=>{
+    page1.style.height = '100vh';
+    page2.style.height = '0vh';
+    
+
+}
+
+/////////////////////////////////////////////////////////////////////
+
+
+
+var _CONTENT = [ 
+	"world", 
+	"developers", 
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element 
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() { 
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
