36a81ab7811ba48f9364f60d9e5e6513320f58b2
WaqarYounus2000 <83652319+WaqarYounus2000@users.noreply.github.com>
2022-09-21 21:52:40 +0500

    .

--- portfolio 2/app.js
+++ portfolio 2/app.js
@@ -11,17 +11,17 @@
 var page1 = document.getElementById('page1ID');
 var page2 = document.getElementById('page2ID');
 
-document.getElementById('button1').onclick = ()=>{
-    page1.style.height = '0vh';
-    page2.style.height = '100vh';
-    
-    
+document.getElementById('button1').onclick = () => {
+	page1.style.height = '0vh';
+	page2.style.height = '100vh';
+
+
 }
 
-document.getElementById('button2').onclick = ()=>{
-    page1.style.height = '100vh';
-    page2.style.height = '0vh';
-    
+document.getElementById('button2').onclick = () => {
+	page1.style.height = '100vh';
+	page2.style.height = '0vh';
+
 
 }
 
@@ -29,9 +29,9 @@ document.getElementById('button2').onclick = ()=>{
 
 
 
-var _CONTENT = [ 
-	"WORLD!", 
-	"DEVELOPERS!", 
+var _CONTENT = [
+	"WORLD!",
+	"DEVELOPERS!",
 ];
 
 // Current sentence being processed
@@ -50,19 +50,19 @@ var _ELEMENT = document.querySelector("#text");
 var _CURSOR = document.querySelector("#cursor");
 
 // Implements typing effect
-function Type() { 
+function Type() {
 	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
 	_ELEMENT.innerHTML = text;
 	_PART_INDEX++;
 
 	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
+	if (text === _CONTENT[_PART]) {
 		// Hide the cursor
 		_CURSOR.style.display = 'none';
 
 		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
+		setTimeout(function () {
 			_INTERVAL_VAL = setInterval(Delete, 80);
 		}, 2000);
 	}
@@ -71,24 +71,24 @@ function Type() {
 // Implements deleting effect
 function Delete() {
 	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
 	_ELEMENT.innerHTML = text;
 	_PART_INDEX--;
 
 	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
+	if (text === '') {
 		clearInterval(_INTERVAL_VAL);
 
 		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
+		if (_PART == (_CONTENT.length - 1))
 			_PART = 0;
 		else
 			_PART++;
-		
+
 		_PART_INDEX = 0;
 
 		// Start to display the next sentence after some time
-		setTimeout(function() {
+		setTimeout(function () {
 			_CURSOR.style.display = 'inline-block';
 			_INTERVAL_VAL = setInterval(Type, 100);
 		}, 200);
@@ -101,4 +101,21 @@ _INTERVAL_VAL = setInterval(Type, 200);
 
 
 ////////////////////// page 3 ///////////////////////////////////////////////////////////////
+
+document.getElementById('sidearrow').onclick = () => {
+
+	document.getElementById('page2_div2ID').style.width = "0%";
+	document.getElementById('page3ID').style.width = "100%";
+
+}
+
+
+document.getElementById('rightarrowID').onclick = () => {
+
+	document.getElementById('page2_div2ID').style.width = "100%";
+	document.getElementById('page3ID').style.width = "0%";
+
+
+}
+
 
